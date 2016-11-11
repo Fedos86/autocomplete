28bf38b74d6f03b55433c9186819d0824ee06a3d
Fedor <chemashkinF@gmail.com>
2016-11-11 21:34:41 +0300

    first

---
+++ autocompleteJSON.js
@@ -0,0 +1,20 @@
+$(function() {
+    // #JSON_array - array which stores JSON
+    $( "#JSON_array" ).autocomplete({ 
+    minLength: 3, 
+    source: function (event, response) { 
+        var matcher = new RegExp( $.ui.autocomplete.escapeRegex(event.term), "i"); 
+        var results = []; 
+
+        for(var i = 0; i < JSON_array.length; i++){ 
+            var result = false; 
+            result = matcher.test(JSON_array[i].name + "#" + JSON_array[i].room + "#"  + JSON_array[i].email); 
+
+            if (result) {
+                results.push(emloyees[i]);
+            }    
+        }
+    response(results);  
+    }
+});
+
